cf46784ba5275d2c7736ed8a4c80aab07b921744
GUchechi <godswilluchendu7@gmail.com>
2024-12-11 14:07:51 +0100

    added community

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,4 @@
-import Community from "../../components/community/community";
+import Community from "../../components/community/Community";  // Capitalized 'C'
 import Hero from "../../components/Hero/Hero";
 import News from "../../components/News/News";
 import Recommended from "../../components/Recommended/Recommended";
