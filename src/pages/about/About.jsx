2490452980f4ad8065940d967ce57aef98ac4380
GUchechi <godswilluchendu7@gmail.com>
2024-12-12 02:15:39 +0100

    added lyrics

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,21 @@
-
-
+import "./About.css";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-container">
+      <div className="glass-bg about-card">
+        <h1>About Music Lyrics Finder</h1>
+        <p>
+          Music Lyrics Finder is a web app designed to help music lovers find
+          their favorite song lyrics easily.
+        </p>
+        <p>
+          Our mission is to provide accurate and up-to-date lyrics to our users,
+          while also promoting the art of music and supporting the artists who
+          create it.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
